2301799f3c786d4092e76bfe7afdecebb61b1ab0
LabBeast <ricktreweek@gmail.com>
2019-02-14 13:29:16 +0200

    Added settings menu. Added pause screen. Hooked up volume settings.

---
+++ Assets/_Src/Systems/Menu_&_Settings/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance;
+
+    public GameObject pausePanel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        
+        //Trigger pause game event
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+
+        //Trigger resume game event
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Src/Systems/Menu_&_Settings/Scripts/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField]private AudioMixer audioMixer;
+    
+    
+    public void MasterVolume(float value)
+    {
+        audioMixer.SetFloat("MasterVolume", value);
+        Debug.Log(value);
+    }
+    
+    public void MusicVolume(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", value);
+        Debug.Log(value);
+    }
+    
+    public void SfxVolume(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", value);
+        Debug.Log(value);
+    }
+}
