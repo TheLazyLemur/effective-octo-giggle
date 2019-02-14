d031cc256a68e694a51359ab0620d0c89d751814
LabBeast <ricktreweek@gmail.com>
2019-02-14 17:00:23 +0200

    Added Preloader scene, Menu Scene and Game Scene

---
+++ Assets/_Src/Systems/Utility/GameOverseer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverseer : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !PauseSystem.Instance.IsPaused())
+        {
+            PauseSystem.Instance.PauseGame();
+        }
+    }
+}
