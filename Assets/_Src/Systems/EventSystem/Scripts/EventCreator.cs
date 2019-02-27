96d2fe75d813afac8df413a6ffcfd1e2553426fc
LabBeast <ricktreweek@gmail.com>
2019-02-27 15:20:22 +0200

    Added the event system to the correct file in the project

---
+++ Assets/_Src/Systems/EventSystem/Scripts/EventCreator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace _JUSTIN.EventSystem.Scripts
+{
+    public class EventCreator : MonoBehaviour
+    {
+        private TestEvents _testEvents;
+
+        private void Awake()
+        {
+            _testEvents = FindObjectOfType<TestEvents>();
+        }
+
+        private void OnEnable()
+        {
+            //SUBSCRIBING TO NON-STATIC EVENTS
+            EventManager.StartListening("SpawnObjects", _testEvents.SpawnObjects);
+            
+            //SUBSCRIBING TO STATIC EVENTS
+            EventManager.StartListening("DestroyObjects", TestEvents.DestroyObjects);
+        }
+
+        private void OnDisable()
+        {
+            //UN-SUBSCRIBING FROM NON-STATIC EVENTS
+            EventManager.StopListening("SpawnObjects", _testEvents.SpawnObjects);
+            
+            //UN-SUBSCRIBING FROM STATIC EVENTS
+            EventManager.StopListening("DestroyObjects", TestEvents.DestroyObjects);
+        }
+    }
+}
