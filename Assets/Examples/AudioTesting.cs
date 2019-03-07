2c65309f535c0c0dd8bbfb02e866c139a7549beb
Dan <danrousseau@protonmail.com>
2019-03-07 15:42:34 +0200

    Updated docs, finsied audio and fade manager as well as an example script for either one

---
+++ Assets/Examples/AudioTesting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTesting : MonoBehaviour
+{
+    [SerializeField] private AudioClip buttonClick;
+    [SerializeField] private AudioClip music1;
+    [SerializeField] private AudioClip music2;
+
+    public bool Test;
+    
+    private void Update()
+    {
+        if(!Test) return;
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AudioManager.instance.PlaySfx(buttonClick);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            AudioManager.instance.PlayMusic(music1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            AudioManager.instance.PlayMusic(music2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            AudioManager.instance.PlayMusicWithFade(music1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            AudioManager.instance.PlayMusicWithFade(music2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            AudioManager.instance.PlayMusicWithCrossFade(music1, 3f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            AudioManager.instance.PlayMusicWithCrossFade(music2, 3f);
+        }
+           
+    }
+}
---
+++ Assets/Examples/FadeTesting.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OVR;
+using UnityEngine;
+
+public class FadeTesting : MonoBehaviour
+{
+    public bool Test;
+    
+    private void Update()
+    {
+        if(!Test) return;
+        
+        TestInput();
+    }
+
+    private  void TestInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            FadeManager.Instance.FadeOut(1, Color.black);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            FadeManager.Instance.FadeIn(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            FadeManager.Instance.FadeOut(1, Color.white);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            FadeManager.Instance.FadeIn(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            FadeManager.Instance.FadeOut(1, Color.yellow, TestFunction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            FadeManager.Instance.FadeIn(1, Color.yellow, TestFunction2);
+        }
+    }
+
+    private void TestFunction()
+    {
+        Debug.Log("Load next scene");
+    }
+    
+    private void TestFunction2()
+    {
+        Debug.Log("Load next scene");
+    }
+}
