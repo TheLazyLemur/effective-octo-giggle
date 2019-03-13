c50a10cced91a192f27df5250ae1f5f6d7cf6d51
Dan Rousseau <DanRousseau@protonmail.com>
2019-03-13 11:53:09 +0200

    updated ETB

---
+++ Assets/_Src/Systems/TriggerBoxComponents/AudioManagerResponseComponent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace EnhancedTriggerbox.Component
+{
+    [AddComponentMenu("")]
+    [Serializable]
+    public class AudioManagerResponseComponent : ResponseComponent
+    {
+        public AudioClip _clipToPlay;
+        public float _transitionTime;
+
+        public enum MethodToCall
+        {
+            PlaySfx,
+            PlayMusic,
+            PlayMusicWithFade,
+            PlayMusicWithCrossFade
+        }
+
+        public MethodToCall methodToCall;
+
+
+        public override void DrawInspectorGUI()
+        {
+#if UNITY_EDITOR
+
+            methodToCall = (MethodToCall) EditorGUILayout.EnumPopup(new GUIContent("", ""), methodToCall);
+
+            _clipToPlay = (AudioClip) UnityEditor.EditorGUILayout.ObjectField(new GUIContent("Sound Clip",
+                "The clip to play"), _clipToPlay, typeof(AudioClip), true);
+
+
+            if (methodToCall == MethodToCall.PlayMusicWithFade || methodToCall == MethodToCall.PlayMusicWithCrossFade)
+            {
+                _transitionTime = EditorGUILayout.FloatField(new GUIContent("Fade time",
+                    "The duration of fading in and out"), _transitionTime);
+            }
+
+
+#endif
+        }
+
+
+        public override void OnAwake()
+        {
+        }
+
+
+        public override bool ExecuteAction()
+        {
+            switch (methodToCall)
+            {
+                case MethodToCall.PlaySfx:
+                    AudioManager.instance.PlaySfx(_clipToPlay);
+                    break;
+                case MethodToCall.PlayMusic:
+                    AudioManager.instance.PlayMusic(_clipToPlay);
+                    break;
+                case MethodToCall.PlayMusicWithFade:
+                    AudioManager.instance.PlayMusicWithFade(_clipToPlay, 1.5f);
+                    break;
+                case MethodToCall.PlayMusicWithCrossFade:
+                    AudioManager.instance.PlayMusicWithCrossFade(_clipToPlay, 1.5f);
+                    break;
+            }
+
+            return true;
+        }
+
+
+        public override void Validation()
+        {
+        }
+    }
+}
---
+++ Assets/_Src/Systems/TriggerBoxComponents/FadeManagerResponseComponent.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace EnhancedTriggerbox.Component
+{
+    [AddComponentMenu("")]
+    [Serializable]
+    public class FadeManagerResponseComponent : ResponseComponent
+    {
+        public float _fadeTime;
+        public Color _color;
+        public GameObject _objToCallMethodOn;
+        public string _functionToCall;
+
+        public enum MethodToCall
+        {
+            FadeIn,
+            FadeInWithMethodCall,
+            FadeOut,
+            FadeOutWithMethodCall
+        }
+
+        public MethodToCall methodToCall;
+
+
+        public override void DrawInspectorGUI()
+        {
+#if UNITY_EDITOR
+
+            methodToCall = (MethodToCall) EditorGUILayout.EnumPopup(new GUIContent("",
+                    ""),
+                methodToCall);
+
+            _fadeTime = EditorGUILayout.FloatField(new GUIContent("Fade time",
+                "The duration of fading in and out"), _fadeTime);
+
+            _color = EditorGUILayout.ColorField(new GUIContent("Fade Color",
+                "The Color of the screen while fading"), _color);
+
+
+            if (methodToCall == MethodToCall.FadeInWithMethodCall || methodToCall == MethodToCall.FadeOutWithMethodCall)
+            {
+                _objToCallMethodOn = (GameObject) UnityEditor.EditorGUILayout.ObjectField(new GUIContent(
+                    "Game object to call method on",
+                    "The method to be called once fade is finished."), _objToCallMethodOn, typeof(GameObject), true);
+
+                _functionToCall = EditorGUILayout.TextField(new GUIContent("Method To Call",
+                    "The duration of fading in and out"), _functionToCall);
+            }
+
+#endif
+        }
+
+
+        public override void OnAwake()
+        {
+        }
+
+
+        public override bool ExecuteAction()
+        {          
+            switch (methodToCall)
+            {
+                case MethodToCall.FadeIn:
+                    FadeManager.Instance.FadeIn(_fadeTime, _color);
+                    break;
+                case MethodToCall.FadeInWithMethodCall:
+                    FadeManager.Instance.FadeIn(_fadeTime,_color);
+                    _objToCallMethodOn.SendMessage(_functionToCall, _fadeTime);
+                    break;
+                case MethodToCall.FadeOut:
+                    Debug.Log(_fadeTime);
+                    FadeManager.Instance.FadeOut(_fadeTime, _color);
+                    break;
+                case MethodToCall.FadeOutWithMethodCall:
+                    FadeManager.Instance.FadeOut(_fadeTime, _color);
+                    _objToCallMethodOn.SendMessage(_functionToCall, _fadeTime);
+                    break;
+            }
+
+            return true;
+        }
+
+
+        public override void Validation()
+        {
+        }
+    }
+}
