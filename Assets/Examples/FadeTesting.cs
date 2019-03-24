ef15bd874ad17dd06af7426c102a72ba5539b169
Dan <danrouseau@protonmail.com>
2019-03-25 00:32:18 +0200

    Improved the save system, less code and more generic

--- Assets/Examples/FadeTesting.cs
+++ Assets/Examples/FadeTesting.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class FadeTesting : MonoBehaviour
 {
     public bool Test;
+
     
     private void Update()
     {
---
+++ Assets/_Src/Systems/Save_System/Rewrite/FileManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class FileManager
+{
+    public static T Load<T>(string filename) where T : new()
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, filename);
+        
+        T output;
+
+        if (File.Exists(filePath))
+        {
+            var dataAsJson = File.ReadAllText(filePath);
+            output = JsonUtility.FromJson<T>(dataAsJson);
+        }
+        else
+        {
+            output = new T();
+        }
+
+        return output;
+    }
+
+    public static void Save<T>(string filename, T content)
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, filename);
+
+        var dataAsJson = JsonUtility.ToJson(content);
+        File.WriteAllText(filePath, dataAsJson);
+
+    }
+    
+}
